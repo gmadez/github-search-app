b8542a9ed8d5bc364815194a94fcfd92f6b67bcf
Hector Magaña <vegtor@Hectors-MacBook-Pro.local>
2022-04-20 23:16:21 -0500

    Prettier added

--- app/types.d.ts
+++ app/types.d.ts
@@ -1,10 +1,10 @@
-export type ResultType = "User" | "Organization";
+export type ResultType = 'User' | 'Organization';
 
 export interface SearchResult {
-    login: string
-    id: string
-    avatar_url?: string
-    url?: string
-    type: ResultType
-    html_url?: string
+  login: string;
+  id: string;
+  avatar_url?: string;
+  url?: string;
+  type: ResultType;
+  html_url?: string;
 }
